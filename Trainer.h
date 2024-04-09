9023ca3f6418a0b7d4ea99db4d4b4d9e2d68326d
tdani7 <tdani7@uis.edu>
2024-04-08 20:09:12 -0500

    fixed a few key issues with wordle code, added time-based stop to prevent training taking too long to complete a generation

--- Trainer.h
+++ Trainer.h
@@ -5,7 +5,7 @@
 namespace ControllerTrainer {
 
     const int generations = 50;  // number of generations
-    const int genSize = 25;  // size of each generation
+    const int genSize = 30;  // size of each generation
     const int bestToKeep = 5;
     const int newAgentsPerGen = genSize - bestToKeep;  // number of fresh controllers to generate for each generation
     const int genLength = 1200;  // number of turns per generation
